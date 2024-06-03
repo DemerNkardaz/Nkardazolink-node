c0a620d2ef0e90d4d757e424c24144570e38404a
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-06-03 10:22:17 +0300

    А

--- index.js
+++ index.js
@@ -4,17 +4,12 @@ const sassMiddleware = require('sass-middleware');
 const markdownIt = require('markdown-it');
 const app = express();
 const md = markdownIt();
+const yaml = require('js-yaml');
+const jsonpath = require('jsonpath');
+const $ = require('jquery');
+const jzsip = require('jszip');
 const port = 3000;
 
-app.use(
-  sassMiddleware({
-    src: path.join(__dirname, 'app', 'styles'), // Путь к исходным файлам SASS
-    dest: path.join(__dirname, 'public'), // Папка, куда будут скомпилированы CSS файлы
-    debug: true,
-    outputStyle: 'compressed',
-    prefix: '/styles' // Префикс для URL, по которому будет доступен скомпилированный CSS
-  })
-);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -22,11 +17,13 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'app'));
 
 
-app.get('/', (req, res) => {
-  res.render('index.page.ejs', {
+app.get('/', (request, resource) => {
+  resource.render('index.page.ejs', {
     title: 'Server-Side Rendering with Node.js',
     message: 'Hello from the server!',
-    req: req
+    request: request,
+    userURL: request.url,
+    navigatorLanguage: request.headers['accept-language']
   });
 });
 
