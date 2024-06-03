82b7aed319b5f34c107e10bcc125a0b930599168
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-06-03 12:15:01 +0300

    А

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
+const fs = require('fs');
 const express = require('express');
+const http = require('http');
 const path = require('path');
-const sassMiddleware = require('sass-middleware');
 const markdownIt = require('markdown-it');
 const app = express();
+const os = require('os');
 const md = markdownIt();
 const yaml = require('js-yaml');
 const jsonpath = require('jsonpath');
 const $ = require('jquery');
 const jzsip = require('jszip');
+const ejs = require('ejs');
+
+
+const currencyConverter = require('currency-converter-lt');
+const CC = (from, to, amount) => new Promise((resolve, reject) => {
+  new currencyConverter({ from, to, amount })
+    .convert()
+    .then(resolve)
+    .catch(reject);
+});
+
+const USD_to_JPY = CC('USD', 'JPY', 100).then(result => console.log(result)).catch(console.error);
+  
 const port = 3000;
 
+const DIR = __dirname;
 
-app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(express.static(path.join(DIR, 'public')));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'app'));
+app.set('views', path.join(DIR, 'app'));
 
 
-app.get('/', (request, resource) => {
-  resource.render('index.page.ejs', {
+app.get('/', async (request, resource) => {
+  const headerTemplate = await ejs.renderFile('app/header.ejs', {
+    userURL: request.url,
+    navigatorLanguage: request.headers['accept-language']
+  });
+
+  const transferedData = {
     title: 'Server-Side Rendering with Node.js',
     message: 'Hello from the server!',
     request: request,
     userURL: request.url,
-    navigatorLanguage: request.headers['accept-language']
-  });
+    navigatorLanguage: request.headers['accept-language'],
+    headerTemplate: headerTemplate
+  }
+
+
+  resource.render('index.page.ejs', transferedData);
 });
 
 app.listen(port, () => {
