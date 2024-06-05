1842c564b7559e72b30b8dac804c60577738531d
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-06-05 16:18:36 +0300

    А

--- index.dev.js
+++ index.dev.js
@@ -4,6 +4,7 @@ console.log(`\x1b[35m[${new Date().toLocaleString().replace(',', '')}] :: 🟪 >
 const readFileAsync = promisify(fs.readFile);
 app.use(compression());
 app.use(express.static(path.join(__PROJECT_DIR__, 'public')));
+app.use(express.static(path.join(__PROJECT_DIR__, 'site.maps')));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.json());
@@ -14,12 +15,13 @@ app.set('views', path.join(__PROJECT_DIR__, 'app'));
 
 async function writeRobots_x_SiteMap() {
   try {
-    const content = `User-agent: *\nSitemap: http://${process.env.HOST}:${process.env.PORT}/sitemap.xml`;
+    const content = `User-agent: *n\nSitemap: http://${process.env.HOST}:${process.env.PORT}/sitemap.index.xml.gz\nSitemap: http://${process.env.HOST}:${process.env.PORT}/sitemap.index.xml`;
     fs.writeFileSync('./robots.txt', content, 'utf-8');
 
     const locations = [
       { url: `http://${process.env.HOST}:${process.env.PORT}/`, lastmod: new Date().toISOString(), changefreq: 'daily', priority: '0.8' },
-      { url: `http://${process.env.HOST}:${process.env.PORT}/sitemap.xml`, lastmod: new Date().toISOString(), changefreq: 'daily', priority: '0.6' },
+      { url: `http://${process.env.HOST}:${process.env.PORT}/sitemap.index.xml.gz`, lastmod: new Date().toISOString(), changefreq: 'daily', priority: '0.6' },
+      { url: `http://${process.env.HOST}:${process.env.PORT}/sitemap.index.xml`, lastmod: new Date().toISOString(), changefreq: 'daily', priority: '0.6' },
       { url: `http://${process.env.HOST}:${process.env.PORT}/wiki/`, lastmod: new Date().toISOString(), changefreq: 'daily', priority: '0.4' },
     ];
     VALID_MODES.forEach(mode => {
@@ -32,40 +34,62 @@ async function writeRobots_x_SiteMap() {
       }
     });
     const getFileExtensions = (urls) => {
-        const extensions = new Set();
-        urls.forEach(url => {
-            const match = url.match(/\.([^.\/\?]+)(?:\?|$)/);
-            if (match) {
-                extensions.add(match[1]);
-            }
-        });
-        return Array.from(extensions);
+      const extensions = new Set();
+      urls.forEach(url => {
+        const match = url.match(/\.([^.\/\?]+)(?:\?|$)/);
+        if (match) {
+          extensions.add(match[1]);
+        }
+      });
+      return Array.from(extensions);
     };
-    
+
     const fileExtensions = getFileExtensions(locations.map(url => url.url));
-    
+
     __NK__.langs.supported.forEach(lang => {
-        locations.forEach(url => {
-            if (!url.url.includes('lang') && !fileExtensions.some(ext => url.url.endsWith(ext))) {
-                const prefix = url.url.includes('/?') ? '&' : '?';
-                locations.push({ url: `${url.url}${prefix}lang=${lang}`, lastmod: new Date().toISOString(), changefreq: 'daily', priority: '0.    6' });
-            }
-        });
+      locations.forEach(url => {
+        if (!url.url.includes('lang') && !fileExtensions.some(ext => url.url.endsWith(ext))) {
+          const prefix = url.url.includes('/?') ? '&' : '?';
+          locations.push({ url: `${url.url}${prefix}lang=${lang}`, lastmod: new Date().toISOString(), changefreq: 'daily', priority: '0.    6' });
+        }
+      });
     });
 
-    const xml = xmlbuilder.create('urlset', { version: '1.0', encoding: 'UTF-8' })
+    const sitemaps = [];
+
+    const sitemapXMLHeaderLinks = xmlbuilder.create('urlset', { version: '1.0', encoding: 'UTF-8' })
       .att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
 
     locations.forEach(location => {
-      const url = xml.ele('url')
+      const url = sitemapXMLHeaderLinks.ele('url')
         .ele('loc', location.url).up()
         .ele('lastmod', location.lastmod).up()
         .ele('changefreq', location.changefreq).up()
         .ele('priority', location.priority);
     });
 
-    const xmlContent = xml.end({ pretty: false });
-    fs.writeFileSync('./sitemap.xml', xmlContent, 'utf-8');
+    sitemaps.push({ url: 'sitemap_headlinks.xml', lastmod: new Date().toISOString(), content: sitemapXMLHeaderLinks.end({ pretty: false })});
+    
+    const sitemapXMLIndex = xmlbuilder.create('sitemapindex', { version: '1.0', encoding: 'UTF-8' })
+      .att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
+
+    sitemaps.forEach(sitemap => {
+      sitemapXMLIndex.ele('sitemap')
+      .ele('loc', `http://${process.env.HOST}:${process.env.PORT}/${sitemap.url}.gz`).up()
+      .ele('lastmod', sitemap.lastmod).up();
+      sitemapXMLIndex.ele('sitemap')
+      .ele('loc', `http://${process.env.HOST}:${process.env.PORT}/${sitemap.url}`).up()
+      .ele('lastmod', sitemap.lastmod).up();
+    });
+    sitemaps.push({ url: 'sitemap.index.xml', lastmod: new Date().toISOString(), content: sitemapXMLIndex.end({ pretty: false })});
+    console.log(sitemapXMLIndex.end({ pretty: false }));
+    sitemaps.forEach(sitemap => {
+      fs.writeFileSync(`./site.maps/${sitemap.url}`, sitemap.content, 'utf-8');
+      zlib.gzip(sitemap.content, (err, zipped) => {
+        if (err) console.error(err);
+        else fs.writeFileSync(`./site.maps/${sitemap.url}.gz`, zipped, 'binary');
+      });
+    });
   } catch (error) {
     console.error(error);
   }
