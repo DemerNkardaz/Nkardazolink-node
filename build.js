cb3131fa4921d2e0cee5371bb44fa2d14d26459c
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-06-07 23:12:52 +0300

    А

--- build.js
+++ build.js
@@ -2,130 +2,26 @@ require('./nk.config.js').config().init(['AppVariables']);
 const { execSync } = require('child_process');
 const fs = require('fs-extra');
 const path = require('path');
-const terser = require('terser');
-const htmlMinifier = require('html-minifier');
-const sass = require('sass');
-const { promisify } = require('util');
-const writeFilePromise = promisify(fs.writeFile);
-const serverSource = './src/serverside';
-const serverDestination = './app';
-const clientSource = './src/clientside';
-const clientDestination = './static/public';
-const handlingExtensions = ['.js', '.html', '.scss', '.css'];
+const { copyFilesAndMinify, createManifest, index, checkForIndex } = require('./server.workers/server/building.files.js');
 
 
 const runArguments = process.argv.slice(2);
 
-const checkForIndex = async () => {
-  try {
-    await fs.access(path.join(__dirname, 'index.js'));
-    execSync('node index.js', { stdio: 'inherit' });
-  } catch (error) {
-    console.error(`[${new Date().toLocaleString().replace(',', '')}] :: 🟥 > Not found [index.js], runnig % builder %`);
-    execSync('node build.js index', { stdio: 'inherit' });
-  }
-}
 
-async function copyFilesAndMinify(sourceDir, destinationDir) {
-  await fs.ensureDir(destinationDir);
-  const entries = await fs.readdir(sourceDir, { withFileTypes: true });
-
-  for (const entry of entries) {
-    const sourcePath = path.join(sourceDir, entry.name);
-    const destinationPath = path.join(destinationDir, entry.name);
-
-    if (entry.isFile()) {
-      if (path.extname(sourcePath) === '.js') {
-        const fileContent = await fs.readFile(sourcePath, 'utf8');
-        const minified = await terser.minify(fileContent, {
-          compress: true,
-          mangle: true,
-          keep_fnames: true,
-          keep_classnames: true
-        });
-        await fs.writeFile(destinationPath, minified.code);
-      } else if (path.extname(sourcePath) === '.html') {
-        const fileContent = await fs.readFile(sourcePath, 'utf8');
-        const minified = htmlMinifier.minify(fileContent, {
-          collapseWhitespace: true,
-          removeComments: true
-        });
-        await fs.writeFile(destinationPath, minified);
-      } else if (path.extname(sourcePath) === '.scss' || path.extname(sourcePath) === '.css') {
-        const result = sass.compile(sourcePath, { style: 'compressed' });
-        await fs.writeFile(destinationPath, result.css.toString());
-      } else if (!handlingExtensions.includes(path.extname(sourcePath))) {
-        await fs.copy(sourcePath, destinationPath);
-      }
-    } else if (entry.isDirectory()) {
-      await copyFilesAndMinify(sourcePath, destinationPath);
-    }
-    console.log(`\x1b[33m[${new Date().toLocaleString().replace(',', '')}] :: 🟨 > [BUILDER] % “${destinationPath}\\${entry.name}” created successfully!\x1b[39m`);
-  }
-}
-
-const createManifest = async (lang, manifest) => {
-  const translate = (obj) => {
-    if (typeof obj !== 'object' || obj === null) {
-      return obj;
-    }
-
-    if (Array.isArray(obj)) {
-      return obj.map(translate);
-    }
-
-    const translated = {};
-    for (const key in obj) {
-      if (typeof obj[key] === 'object' && obj[key] !== null) {
-        translated[key] = translate(obj[key]);
-      } else if (key === lang) {
-        return obj[key];
-      } else if (key === 'en' && !(lang in obj)) {
-        return obj[key];
-      } else {
-        translated[key] = obj[key];
-      }
-    }
-
-    return translated;
-  };
-
-  const translatedManifest = translate(manifest);
-  const outputPath = path.join(__dirname, `static/public/manifest/manifest.${lang}.webmanifest`);
-  const minifiedManifest = JSON.stringify({ lang, ...translatedManifest }, null, 0);
-  await fs.writeFile(outputPath, minifiedManifest, 'utf-8');
-  console.log(`\x1b[33m[${new Date().toLocaleString().replace(',', '')}] :: 🟨 > [BUILDER] % Manifest for [${lang.toUpperCase()}] created successfully!\x1b[39m`);
-};
-
-
-async function index() {
-  const sourcePath = path.join(__dirname, 'index.dev.js');
-  const destinationPath = path.join(__dirname, 'index.js');
-
-  const fileContent = await fs.readFile(sourcePath, 'utf8');
-  const minified = await terser.minify(fileContent, {
-    compress: true,
-    mangle: true,
-    keep_fnames: true,
-    keep_classnames: true
-  });
-  await writeFilePromise(destinationPath, minified.code);
-
-  console.log(`\x1b[35m[${new Date().toLocaleString().replace(',', '')}] :: 🟪 > [BUILDER] :: “${destinationPath}” Index file builded\x1b[39m`);
-}
 async function build() {
   try {
-    await copyFilesAndMinify(clientSource, clientDestination);
-    await copyFilesAndMinify(serverSource, serverDestination)
+    await copyFilesAndMinify(path.join(__PROJECT_DIR__, 'src/clientside'), path.join(__PROJECT_DIR__, 'static/public'));
+    await copyFilesAndMinify(path.join(__PROJECT_DIR__, 'src/serverside'), path.join(__PROJECT_DIR__, 'app'))
       .then(() => console.log(`\x1b[32m[${new Date().toLocaleString().replace(',', '')}] :: 🟩 > [BUILDER] :: Files copied and minified successfully\x1b[39m`))
       .catch(error => console.error(`[${new Date().toLocaleString().replace(',', '')}] :: 🟥 > Error during copy and minify: ${error.message}`));
-    const manifestOutput = path.join(__dirname, 'static/public/manifest/');
+    
+    const manifestOutput = path.join(__PROJECT_DIR__, 'static/public/manifest/');
     if (!fs.existsSync(manifestOutput)) fs.mkdirSync(manifestOutput, { recursive: true });
     const { MANIFEST } = await require('./app/templates/manifest_template.js');
-    const createManifestPromises = await __NK__.langs.supported.map(lang => createManifest(lang, MANIFEST));
+    const createManifestPromises = await __NK__.langs.supported.map(lang => createManifest(__PROJECT_DIR__, lang, MANIFEST));
     await Promise.all(createManifestPromises)
       .then(() => console.log(`\x1b[32m[${new Date().toLocaleString().replace(',', '')}] :: 🟩 > [BUILDER] :: All manifests created successfully\x1b[39m`)).catch(error => console.error(`[${new Date().toLocaleString().replace(',', '')}] :: 🟥 > Error during build: ${error.message}`));
-
+    require('./server.workers/server/sitemap.gen.js').generateSiteMaps(__PROJECT_DIR__);
   } catch (error) {
     console.error(`[${new Date().toLocaleString().replace(',', '')}] :: 🟥 > Error during build: ${error.message}`);
   }
@@ -134,9 +30,9 @@ async function build() {
 const BUILING_PROMISE = new Promise((resolve, reject) => {
   try {
     (async () => {
-      if (runArguments.includes('start')) await checkForIndex();
+      if (runArguments.includes('start')) await checkForIndex(__PROJECT_DIR__);
       if (!runArguments.includes('index_rebuild')) await build();
-      if (runArguments.includes('index') || runArguments.includes('index_rebuild')) await index();
+      if (runArguments.includes('index') || runArguments.includes('index_rebuild')) await index(__PROJECT_DIR__);
       resolve();
     })();
   } catch (error) {
---
+++ server.workers/server/building.files.js
@@ -0,0 +1,109 @@
+require('../../nk.config.js').config().init(['AppVariables']);
+const { execSync } = require('child_process');
+const fs = require('fs-extra');
+const path = require('path');
+const { promisify } = require('util');
+const writeFilePromise = promisify(fs.writeFile);
+const terser = require('terser');
+const htmlMinifier = require('html-minifier');
+const sass = require('sass');
+const handlingExtensions = ['.js', '.html', '.scss', '.css'];
+
+const checkForIndex = async (sourcePath) => {
+  try {
+    await fs.access(path.join(sourcePath, 'index.js'));
+    execSync('node index.js', { stdio: 'inherit' });
+  } catch (error) {
+    console.error(`[${new Date().toLocaleString().replace(',', '')}] :: 🟥 > Not found [index.js], runnig % builder %`);
+    execSync('node build.js index', { stdio: 'inherit' });
+  }
+}
+
+async function copyFilesAndMinify(sourceDir, destinationDir) {
+  await fs.ensureDir(destinationDir);
+  const entries = await fs.readdir(sourceDir, { withFileTypes: true });
+
+  for (const entry of entries) {
+    const sourcePath = path.join(sourceDir, entry.name);
+    const destinationPath = path.join(destinationDir, entry.name);
+
+    if (entry.isFile()) {
+      if (path.extname(sourcePath) === '.js') {
+        const fileContent = await fs.readFile(sourcePath, 'utf8');
+        const minified = await terser.minify(fileContent, {
+          compress: true,
+          mangle: true,
+          keep_fnames: true,
+          keep_classnames: true
+        });
+        await fs.writeFile(destinationPath, minified.code);
+      } else if (path.extname(sourcePath) === '.html') {
+        const fileContent = await fs.readFile(sourcePath, 'utf8');
+        const minified = htmlMinifier.minify(fileContent, {
+          collapseWhitespace: true,
+          removeComments: true
+        });
+        await fs.writeFile(destinationPath, minified);
+      } else if (path.extname(sourcePath) === '.scss' || path.extname(sourcePath) === '.css') {
+        const result = sass.compile(sourcePath, { style: 'compressed' });
+        await fs.writeFile(destinationPath, result.css.toString());
+      } else if (!handlingExtensions.includes(path.extname(sourcePath))) {
+        await fs.copy(sourcePath, destinationPath);
+      }
+    } else if (entry.isDirectory()) {
+      await copyFilesAndMinify(sourcePath, destinationPath);
+    }
+    console.log(`\x1b[33m[${new Date().toLocaleString().replace(',', '')}] :: 🟨 > [BUILDER] % “${destinationPath}\\${entry.name}” created successfully!\x1b[39m`);
+  }
+}
+
+const createManifest = async (sourcePath, lang, manifest) => {
+  const translate = (obj) => {
+    if (typeof obj !== 'object' || obj === null) {
+      return obj;
+    }
+
+    if (Array.isArray(obj)) {
+      return obj.map(translate);
+    }
+
+    const translated = {};
+    for (const key in obj) {
+      if (typeof obj[key] === 'object' && obj[key] !== null) {
+        translated[key] = translate(obj[key]);
+      } else if (key === lang) {
+        return obj[key];
+      } else if (key === 'en' && !(lang in obj)) {
+        return obj[key];
+      } else {
+        translated[key] = obj[key];
+      }
+    }
+
+    return translated;
+  };
+
+  const translatedManifest = translate(manifest);
+  const outputPath = path.join(sourcePath, `static/public/manifest/manifest.${lang}.webmanifest`);
+  const minifiedManifest = JSON.stringify({ lang, ...translatedManifest }, null, 0);
+  await fs.writeFile(outputPath, minifiedManifest, 'utf-8');
+  console.log(`\x1b[33m[${new Date().toLocaleString().replace(',', '')}] :: 🟨 > [BUILDER] % Manifest for [${lang.toUpperCase()}] created successfully!\x1b[39m`);
+};
+
+async function index(sourcePath) {
+  const destinationPath = path.join(sourcePath, 'index.js');
+  sourcePath = path.join(sourcePath, 'index.dev.js');
+
+  const fileContent = await fs.readFile(sourcePath, 'utf8');
+  const minified = await terser.minify(fileContent, {
+    compress: true,
+    mangle: true,
+    keep_fnames: true,
+    keep_classnames: true
+  });
+  await writeFilePromise(destinationPath, minified.code);
+
+  console.log(`\x1b[35m[${new Date().toLocaleString().replace(',', '')}] :: 🟪 > [BUILDER] :: “${destinationPath}” Index file builded\x1b[39m`);
+}
+
+module.exports = { copyFilesAndMinify, createManifest, index, checkForIndex };
