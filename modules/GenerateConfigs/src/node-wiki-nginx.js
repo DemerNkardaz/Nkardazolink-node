59e0817cac2fa6412c3403ebc9043191f0794672
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-06-20 00:54:17 +0300

    А

--- modules/GenerateConfigs/src/node-wiki-nginx.js
+++ modules/GenerateConfigs/src/node-wiki-nginx.js
@@ -1,6 +1,3 @@
-// Test force git clone
-// TEST 2
-// THE FINAL TEST
 const nginxErrors = {
   400: ['400 – Bad Request', 'Your browser sent a request that this server could not understand.'],
   401: ['401 – Unauthorized', 'You are not authorized to view the page. Please log in or check your credentials.'],
