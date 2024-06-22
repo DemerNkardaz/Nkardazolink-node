485897d249778b2caca85e57867bc9f325aab6c3
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-06-22 21:43:37 +0300

    А

--- src/clientside/script/wiki_controls.js
+++ src/clientside/script/wiki_controls.js
@@ -3,12 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
   if (isArticleExpanded) {
     const article = document.querySelector('.nw-article-page');
     const articleParents = document.querySelectorAll('.nw-root-container__main-block, .nw-root-container');
-    const expandButton = document.querySelector('[data-icon="fullscreen"]');
+    const expandButton = document.querySelector('.nw-page-action__expand-page[data-action="expand-article"]');
     articleParents.forEach(parent => parent.style.transition = 'none');
     article.classList.add('nw-article-page--expanded');
     expandButton.setAttribute('data-icon', 'fullscreen_exit');
     setTimeout(() => articleParents.forEach(parent => parent.removeAttribute('style')), 100);
   }
+
+  const editPageButton = document.querySelector('.nw-page-action__edit[data-action="edit-article"]');
+  editPageButton.setAttribute('href', `${window.location.href.split('?')[0]}?action=edit`);
 });
 
 document.addEventListener('click', function (e) {
