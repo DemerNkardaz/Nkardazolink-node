f6d8d7afe8263c1fd105338d2fb8995a5c8d62fc
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-06-22 21:10:24 +0300

    А

--- modules/PostProcessor/src/PostProcessor.js
+++ modules/PostProcessor/src/PostProcessor.js
@@ -1,7 +1,18 @@
+const htmlMinifier = require('html-minifier');
+
+const htmlMinifyOptions = {
+  removeAttributeQuotes: true,
+  collapseWhitespace: true,
+  removeComments: true,
+  minifyJS: true,
+  minifyCSS: true
+}
 
 async function PostProcessor(page, data) {
   page = await StringHandling(page, data);
   page = eval('`' + page + '`');
+
+  page = htmlMinifier.minify(page, htmlMinifyOptions);
   return page;
 } 
 
