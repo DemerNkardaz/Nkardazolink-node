ad056c68274c90d13f175b0b8208d76b78a18895
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-06-28 08:19:34 +0300

    А

--- modules/WikiMarkup/src/WikiMarkup.js
+++ modules/WikiMarkup/src/WikiMarkup.js
@@ -1,6 +1,24 @@
 const space2underline = (str) => str.replace(/\s/g, '_');
 
 const localisedTitles = (obj) => `${obj.text} (${obj.wikiSource ? `<a href="https://${obj.wikiSource[2]}.wikipedia.org/wiki/${obj.wikiSource[1]}">${obj.wikiSource[0]}</a>&nbsp;` : ''}<span lang="${obj.lang}" class="nw-font-${obj.lang}">${obj.origin}</span>${obj.add ? `, ${obj.add}` : ''})`;
+const localSpan = (lang, text) => `<span lang="${lang}" class="nw-font-${lang}">${text}</span>`;
+const isoLanguages = [
+  "aa", "ab", "ae", "af", "ak", "am", "an", "ar", "as", "av", "ay", "az", "ba",
+  "be", "bg", "bh", "bi", "bm", "bn", "bo", "br", "bs", "ca", "ce", "ch", "co",
+  "cr", "cs", "cu", "cv", "cy", "da", "de", "dv", "dz", "ee", "el", "en", "eo",
+  "es", "et", "eu", "fa", "ff", "fi", "fj", "fo", "fr", "fy", "ga", "gd", "gl",
+  "gn", "gu", "gv", "ha", "he", "hi", "ho", "hr", "ht", "hu", "hy", "hz", "ia",
+  "id", "ie", "ig", "ii", "ik", "io", "is", "it", "iu", "ja", "jv", "ka", "kg",
+  "ki", "kj", "kk", "kl", "km", "kn", "ko", "kr", "ks", "ku", "kv", "kw", "ky",
+  "la", "lb", "lg", "li", "ln", "lo", "lt", "lu", "lv", "mg", "mh", "mi", "mk",
+  "ml", "mn", "mr", "ms", "mt", "my", "na", "nb", "nd", "ne", "ng", "nl", "nn",
+  "no", "nr", "nv", "ny", "oc", "oj", "om", "or", "os", "pa", "pi", "pl", "ps",
+  "pt", "qu", "rm", "rn", "ro", "ru", "rw", "sa", "sc", "sd", "se", "sg", "si",
+  "sk", "sl", "sm", "sn", "so", "sq", "sr", "ss", "st", "su", "sv", "sw", "ta",
+  "te", "tg", "th", "ti", "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw", "ty",
+  "ug", "uk", "ur", "uz", "ve", "vi", "vo", "wa", "wo", "xh", "yi", "yo", "za",
+  "zh", "zu"
+];
 
 
 
@@ -24,6 +42,13 @@ const allowedTags = [
 class WikiMarkup {
   constructor(options) {
     this.marks = {
+      localedText: [
+        /\{\{(\w\w):([^{\}]+)\}\}/g,
+        (match, prefix, text) => {
+          if (isoLanguages.includes(prefix)) return localSpan(prefix, text);
+          else return match;
+        }
+      ],
       nihongo: [
         /\{\{(nihon-go|нихон-го):([^{\}]+)\}\}/g,
         (match, prefix, options) => {
@@ -31,7 +56,7 @@ class WikiMarkup {
           let localisedText, originText, additional, wikiSource;
 
           if (optionsArray.length > 0) {
-            localisedText = optionsArray[0];
+            localisedText = optionsArray[0] ?? null;
             originText = optionsArray[1];
             additional = optionsArray[2] ?? null;
           }
