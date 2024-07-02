6d21dc43f5890b0cfae7f23b58bcd95b0fbe2939
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-07-02 07:14:58 +0300

    А

--- modules/WikiTemplate/src/WikiTemplate.js
+++ modules/WikiTemplate/src/WikiTemplate.js
@@ -1,11 +1,4 @@
-const systemTemplates = {};
-
-systemTemplates.nihongoRU = {
-  name: 'нихон-го', template: '<let:text/> (<a href="https://ru.wikipedia.org/wiki/Японский_язык" target="_blank">яп.</a> <span lang="ja" class="nw-font-ja"><let:kanji/></span><let:additional/>)'};
-
-
-
-
+/*
 class WikiTemplate {
   constructor(options) {
 
@@ -25,7 +18,41 @@ class WikiTemplate {
 
 
   #handleTemplate(templateName) {
-    
+
+  }
+}
+*/
+
+const systemTemplates = {
+  'пример-шаблона': `
+    <let var='text' val='Default text'/>
+    <let var='kanji'/>
+    <let var='additional'/>
+
+    <template>
+      <use var='text'/> (<a href="https://en.wikipedia.org/wiki/Japanese_language" target="_blank">Japanese:</a> <span lang="ja" class="nw-font-ja"><use var='kanji'/></span><use var='additional' insertAfter=", "/>)
+    </template>`
+};
+class WikiTemplate {
+  constructor(options) {
+
+  }
+
+  async render(markup) {
+    try {
+      let result;
+
+
+    } catch (err) {
+      console.log(err);
+    } finally {
+      return await markup;
+    }
+  }
+
+
+  #handleTemplate(templateName) {
+
   }
 }
 
