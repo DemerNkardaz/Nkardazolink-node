8d722f1dbcebf7019398ae878b70d5d95304c12f
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-07-17 21:37:14 +0300

    А

--- extensions/ImageHandler/src/ImageRoute.js
+++ extensions/ImageHandler/src/ImageRoute.js
@@ -5,41 +5,45 @@ const path = require('path');
 const imageRouter = express.Router();
 
 async function processImage(request, response, next, localFile = false) {
-  try {
-    const worker = new Worker(path.resolve(__dirname, './ImageTask.js'));
-    const workerRequest = { query: request.query ?? {}, params: request.params ?? {}, url: request.url };
-    const messageData = { __PROJECT_DIR__, serverConfig, workerRequest };
+  const isFileTypeValid = serverConfig.allowedFileTypes.images.includes(path.extname(request.path));
+  if (isFileTypeValid) {
+    try {
+      const worker = new Worker(path.resolve(__dirname, './ImageTask.js'));
+      const workerRequest = { query: request.query ?? {}, params: request.params ?? {}, url: request.url };
+      const messageData = { __PROJECT_DIR__, serverConfig, workerRequest };
 
-    if (localFile) messageData.localFile = true;
+      if (localFile) messageData.localFile = true;
 
-    worker.postMessage(messageData);
+      worker.postMessage(messageData);
 
-    worker.on('message', (message) => {
-      if (message.error) {
-        const error = new Error(message.error);
-        error.status = 404;
+      worker.on('message', (message) => {
+        if (message.error) {
+          const error = new Error(message.error);
+          error.status = 404;
+          next(error);
+        } else {
+          const imageBuffer = Buffer.from(message.imageBuffer);
+          response.contentType(message.mimeType);
+          response.send(imageBuffer);
+        }
+      });
+
+      worker.on('error', (error) => {
+        console.error('Error processing image:', error);
         next(error);
-      } else {
-        const imageBuffer = Buffer.from(message.imageBuffer);
-        response.contentType(message.mimeType);
-        response.send(imageBuffer);
-      }
-    });
+      });
 
-    worker.on('error', (error) => {
+      worker.on('exit', (code) => {
+        if (code !== 0) {
+          console.error(`Worker stopped with exit code ${code}`);
+        }
+      });
+    } catch (error) {
       console.error('Error processing image:', error);
       next(error);
-    });
-
-    worker.on('exit', (code) => {
-      if (code !== 0) {
-        console.error(`Worker stopped with exit code ${code}`);
-      }
-    });
-  } catch (error) {
-    console.error('Error processing image:', error);
-    next(error);
+    }
   }
+  else next();
 }
 
 imageRouter.get('/local/images/*', async (request, response, next) => await processImage(request, response, next, true));
