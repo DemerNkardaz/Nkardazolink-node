2bd87ea9a4d92a0cff0d1a760f0bf28ce5eeda28
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2024-07-17 20:47:55 +0300

    А

--- extensions/ImageHandler/src/ImageRoute.js
+++ extensions/ImageHandler/src/ImageRoute.js
@@ -4,11 +4,15 @@ const path = require('path');
 
 const imageRouter = express.Router();
 
-imageRouter.get('/local/images/*', async (request, response, next) => {
+async function processImage(request, response, next, localFile = false) {
   try {
     const worker = new Worker(path.resolve(__dirname, './ImageTask.js'));
     const workerRequest = { query: request.query ?? {}, params: request.params ?? {}, url: request.url };
-    worker.postMessage({ __PROJECT_DIR__, serverConfig, workerRequest, localFile: true });
+    const messageData = { __PROJECT_DIR__, serverConfig, workerRequest };
+
+    if (localFile) messageData.localFile = true;
+
+    worker.postMessage(messageData);
 
     worker.on('message', (message) => {
       if (message.error) {
@@ -36,41 +40,9 @@ imageRouter.get('/local/images/*', async (request, response, next) => {
     console.error('Error processing image:', error);
     next(error);
   }
-});
+}
 
-
-imageRouter.get('/shared/images/:imageFileName', async (request, response, next) => {
-  try {
-    const worker = new Worker(path.resolve(__dirname, './ImageTask.js'));
-    const workerRequest = { query: request.query ?? {}, params: request.params ?? {}, url: request.url };
-    worker.postMessage({ __PROJECT_DIR__, serverConfig, workerRequest });
-
-    worker.on('message', (message) => {
-      if (message.error) {
-        const error = new Error(message.error);
-        error.status = 404;
-        next(error);
-      } else {
-        const imageBuffer = Buffer.from(message.imageBuffer);
-        response.contentType(message.mimeType);
-        response.send(imageBuffer);
-      }
-    });
-
-    worker.on('error', (error) => {
-      console.error('Error processing image:', error);
-      next(error);
-    });
-
-    worker.on('exit', (code) => {
-      if (code !== 0) {
-        console.error(`Worker stopped with exit code ${code}`);
-      }
-    });
-  } catch (error) {
-    console.error('Error processing image:', error);
-    next(error);
-  }
-});
+imageRouter.get('/local/images/*', async (request, response, next) => await processImage(request, response, next, true));
+imageRouter.get('/shared/images/:imageFileName', async (request, response, next) => await processImage(request, response, next));
 
 module.exports = { imageRouter };
